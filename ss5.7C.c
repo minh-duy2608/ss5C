fa29e7d12036c650664276ea2dac005b381dbb19
minh-duy2608 <nguyenduy26806@gmail.com>
2024-11-26 00:54:22 +0700

    Add files via upload

---
+++ ss5.7C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int a,b,c,lonNhat;
+	printf("Nhap 2 so nguyen duong: ");
+	scanf("%d %d",a,b);
+	while(b != 0){
+		b=c;
+		a%b == b;
+		a=c;
+			if(a%b==0){
+		printf("Uoc chung lon nhat cua %d va %d la: %d ", a,b,lonNhat);
+
+	}
+}
+    return 0;
+}
